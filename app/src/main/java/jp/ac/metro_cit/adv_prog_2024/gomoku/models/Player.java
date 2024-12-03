251bbaf09ffb487234ad9712900be7425b5697c1
RyuseiHoshi <s24032@g.metro-cit.ac.jp>
2024-12-03 04:51:20 +0000

    style: Stone.javaとPlayer.javaにjavadocを追記し、Stone.java ln10 の空改行を削除

--- app/src/main/java/jp/ac/metro_cit/adv_prog_2024/gomoku/models/Player.java
+++ app/src/main/java/jp/ac/metro_cit/adv_prog_2024/gomoku/models/Player.java
@@ -1,18 +1,40 @@
 package jp.ac.metro_cit.adv_prog_2024.gomoku.models;
 
+/**
+ * プレイヤークラス date: 2024/12/03
+ *
+ * @author 6306 hoshi
+ * @version 1.0
+ */
 public class Player {
   private StoneColor color;
   private String name;
 
+  /**
+   * 引数(型)
+   *
+   * @param color プレイヤーが保有する石の色(enum)
+   * @param name プレイヤー名(String)
+   */
   public Player(StoneColor color, String name) {
     this.color = color;
     this.name = name;
   }
 
+  /**
+   * プレイヤーが保有する石の色を返す。
+   *
+   * @return 対象のプレイヤーが保有する石の色
+   */
   public StoneColor getColor() {
     return color;
   }
 
+  /**
+   * プレイヤー命を返す。
+   *
+   * @return 対象のプレイヤー命
+   */
   public String getName() {
     return name;
   }
--- app/src/main/java/jp/ac/metro_cit/adv_prog_2024/gomoku/models/Stone.java
+++ app/src/main/java/jp/ac/metro_cit/adv_prog_2024/gomoku/models/Stone.java
@@ -1,25 +1,52 @@
 package jp.ac.metro_cit.adv_prog_2024.gomoku.models;
 
+/**
+ * 石クラス date: 2024/12/03
+ *
+ * @author 6306 hoshi
+ * @version 1.0
+ */
 public class Stone {
-
   private StoneColor color;
   private int posX;
   private int posY;
 
+  /**
+   * 引数(型)
+   *
+   * @param color 石の色(enum)`
+   * @param posX 石のX座標(int)
+   * @param posY 石のY座標(int)
+   */
   public Stone(StoneColor color, int posX, int posY) {
     this.color = color;
     this.posX = posX;
     this.posY = posY;
   }
 
+  /**
+   * 石の色を返す。
+   *
+   * @return 対象の石の色
+   */
   public StoneColor getColor() {
     return color;
   }
 
+  /**
+   * 石のX座標を返す。
+   *
+   * @return 対象の石のX座標
+   */
   public int getPosX() {
     return posX;
   }
 
+  /**
+   * 石のY座標を返す。
+   *
+   * @return 対象の石のY座標
+   */
   public int getPosY() {
     return posY;
   }
