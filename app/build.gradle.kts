84713bad45eef44d8911f635e247b6b0ffa4b63e
CaffeinePower <cffnpwr@gmail.com>
2024-12-10 05:14:55 +0000

    refactor: :fire: 不要なspotlessルールを削除

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,7 +91,6 @@ spotless {
         cleanthat()
         formatAnnotations()
         googleJavaFormat()
-        indentWithSpaces(2)
         importOrder("java|javax", "com.acme", "", "\\#com.acme", "\\#")
     }
 }
