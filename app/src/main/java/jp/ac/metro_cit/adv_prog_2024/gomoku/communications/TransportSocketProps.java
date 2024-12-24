f61d6e3e79c46bd3f3df890913cbb124828c5251
shrz <shrz1xyz@gmail.com>
2024-12-24 04:34:59 +0000

    feat: broadcastの宛先ポートをインスタンス生成時に指定するように変更

--- app/src/main/java/jp/ac/metro_cit/adv_prog_2024/gomoku/communications/TransportSocketProps.java
+++ app/src/main/java/jp/ac/metro_cit/adv_prog_2024/gomoku/communications/TransportSocketProps.java
@@ -10,6 +10,7 @@ import jp.ac.metro_cit.adv_prog_2024.gomoku.interfaces.SocketProps;
  * @param address ソケットに紐づけるIPアドレス
  * @param port ソケットに紐付けるポート番号
  * @param subPort broadcastを受信するためのポート
+ * @param targetPort broadcastの送信先のポート
  * @author A. Kokubo
  */
 public record TransportSocketProps(@Nullable String address, int port, int subPort, int targetPort)
