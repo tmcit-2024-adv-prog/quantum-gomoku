d93e5b3d2c70f8e8eb57fffc08935b725e0a919d
RyuseiHoshi <121078372+RyuseiHoshi@users.noreply.github.com>
2025-01-04 20:24:56 +0000

    refactor: タイプミスの修正

--- app/src/test/java/jp/ac/metro_cit/adv_prog_2024/gomoku/StoneTest.java
+++ app/src/test/java/jp/ac/metro_cit/adv_prog_2024/gomoku/StoneTest.java
@@ -21,8 +21,8 @@ public class StoneTest {
     Stone teststone = new Stone(testcolor, testposition);
 
     // 初期状態でcolorとpositionが正しく設定されているか確認
-    assertEquals(color, stone.getColor(), "teststoneの色が正しく設定されていません");
-    assertEquals(position, stone.getPosition(), "teststoneの位置が正しく設定されていません");
+    assertEquals(testcolor, stone.getColor(), "teststoneの色が正しく設定されていません");
+    assertEquals(testposition, stone.getPosition(), "teststoneの位置が正しく設定されていません");
   }
 
   @Test
