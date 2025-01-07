af95e8c52bb28aa890f94cecb62f221e1ededaf0
CaffeinePower <86540016+cffnpwr@users.noreply.github.com>
2025-01-07 20:42:42 +0900

    ci: 🚑 CIでのイベント名が正しく取得できていなかったので修正 (#81)
    * ci: :ambulance: CIでのイベント名が正しく取得できていなかったので修正
    
    * ci: :zap: Spotbugsが失敗したときはCIの実行結果を失敗にする
    
    * style: :art: Suppress Spotbugs

--- app/src/main/java/jp/ac/metro_cit/adv_prog_2024/gomoku/models/TransportTarget.java
+++ app/src/main/java/jp/ac/metro_cit/adv_prog_2024/gomoku/models/TransportTarget.java
@@ -3,4 +3,7 @@ package jp.ac.metro_cit.adv_prog_2024.gomoku.models;
 import java.net.InetAddress;
 import javax.annotation.Nonnull;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+@SuppressFBWarnings(value = {"EI_EXPOSE_REP", "EI_EXPOSE_REP2"})
 public record TransportTarget(@Nonnull InetAddress address, int port) {}
