db175f877bac0bee2090f7f562a3349bf0234259
RyuseiHoshi <s24032@g.metro-cit.ac.jp>
2025-01-07 00:58:27 +0000

    refactor: lowerCamelCaseで変数を宣言

--- app/src/test/java/jp/ac/metro_cit/adv_prog_2024/gomoku/StoneTest.java
+++ app/src/test/java/jp/ac/metro_cit/adv_prog_2024/gomoku/StoneTest.java
@@ -16,109 +16,109 @@ public class StoneTest {
   @DisplayName("[正常系] stoneオブジェクトの初期状態が正しく設定される")
   void testConstructedStoneInitialValue() {
     // Stoneに関する構成情報の作成
-    StoneColor testcolor = StoneColor.BLACK;
-    Vector2D testposition = new Vector2D(3, 4);
-    Stone teststone = new Stone(testcolor, testposition);
+    StoneColor testColor = StoneColor.BLACK;
+    Vector2D testPosition = new Vector2D(3, 4);
+    Stone testStone = new Stone(testColor, testPosition);
 
     // 初期状態でcolorとpositionが正しく設定されているか確認
-    assertEquals(testcolor, teststone.getColor(), "teststoneの色が正しく設定されていません");
-    assertEquals(testposition, teststone.getPosition(), "teststoneの位置が正しく設定されていません");
+    assertEquals(testColor, testStone.getColor(), "石の色が正しく設定されていません");
+    assertEquals(testPosition, testStone.getPosition(), "石の位置が正しく設定されていません");
   }
 
   @Test
   @DisplayName("[正常系] 同じ構成情報を持つ2つのstoneオブジェクトを比較したとき、equalsでtrueを返す")
   void testStoneEquals() {
     // 同じ構成情報を持つstoneオブジェクトを作成
-    StoneColor testcolor = StoneColor.BLACK;
-    Vector2D testposition = new Vector2D(1, 1);
+    StoneColor testColor = StoneColor.BLACK;
+    Vector2D testPosition = new Vector2D(1, 1);
     // 比較対象1
-    Stone teststone1 = new Stone(testcolor, testposition);
+    Stone testStone1 = new Stone(testColor, testPosition);
     // 比較対象2
-    Stone teststone2 = new Stone(testcolor, testposition);
+    Stone testStone2 = new Stone(testColor, testPosition);
 
-    // 同じ色と位置の構成情報を持つため、teststone1とteststone2の比較においてequalsでtrueとなるべき
-    assertTrue(teststone1.equals(teststone2), "同じ色と位置を持つstoneオブジェクトがequalsでtrueとならない");
+    // 同じ色と位置の構成情報を持つため、testStone1とtestStone2の比較においてequalsでtrueとなるべき
+    assertTrue(testStone1.equals(testStone2), "同じ色と位置を持つstoneオブジェクトがequalsでtrueとならない");
   }
 
   @Test
   @DisplayName("[異常系] 異なる色の構成情報を持つ2つのStoneオブジェクトを比較したとき、equalsでfalseを返す")
   void testStoneEqualsDifferentColor() {
     // 色が異なる構成情報を持つstoneオブジェクトを作成
-    StoneColor testcolor1 = StoneColor.BLACK;
-    StoneColor testcolor2 = StoneColor.WHITE;
-    Vector2D testposition = new Vector2D(2, 2);
+    StoneColor testColor1 = StoneColor.BLACK;
+    StoneColor testColor2 = StoneColor.WHITE;
+    Vector2D testPosition = new Vector2D(2, 2);
     // 比較対象1
-    Stone teststone1 = new Stone(testcolor1, testposition);
+    Stone testStone1 = new Stone(testColor1, testPosition);
     // 比較対象2
-    Stone teststone2 = new Stone(testcolor2, testposition);
+    Stone testStone2 = new Stone(testColor2, testPosition);
 
-    // 異なる色の構成情報を持つため、teststone1とteststone2の比較においてequalsでfalseとなるべき
-    assertFalse(teststone1.equals(teststone2), "異なる色を持つstoneオブジェクトがequalsでfalseとならない");
+    // 異なる色の構成情報を持つため、testStone1とtestStone2の比較においてequalsでfalseとなるべき
+    assertFalse(testStone1.equals(testStone2), "異なる色を持つstoneオブジェクトがequalsでfalseとならない");
   }
 
   @Test
-  @DisplayName("[異常系] 異なる位置の構成情報を持つ2つのStoneオブジェクトを比較したとき、equalsでfalseを返す")
+  @DisplayName("[異常系] 異なる位置の構成情報を持つ2つのstoneオブジェクトを比較したとき、equalsでfalseを返す")
   void testStoneEqualsDifferentPosition() {
     // 位置が異なる構成情報を持つstoneオブジェクトを作成
-    StoneColor testcolor = StoneColor.BLACK;
-    Vector2D testposition1 = new Vector2D(1, 1);
-    Vector2D testposition2 = new Vector2D(1, 2);
+    StoneColor testColor = StoneColor.BLACK;
+    Vector2D testPosition1 = new Vector2D(1, 1);
+    Vector2D testPosition2 = new Vector2D(1, 2);
     // 比較対象1
-    Stone teststone1 = new Stone(testcolor, testposition1);
+    Stone testStone1 = new Stone(testColor, testPosition1);
     // 比較対象2
-    Stone teststone2 = new Stone(testcolor, testposition2);
+    Stone testStone2 = new Stone(testColor, testPosition2);
 
-    // 異なる位置の構成情報を持つため、teststone1とteststone2の比較においてequalsでfalseとなるべき
-    assertFalse(teststone1.equals(teststone2), "異なる位置を持つstoneオブジェクトがequalsでfalseとならない");
+    // 異なる位置の構成情報を持つため、testStone1とtestStone2の比較においてequalsでfalseとなるべき
+    assertFalse(testStone1.equals(testStone2), "異なる位置を持つstoneオブジェクトがequalsでfalseとならない");
   }
 
   @Test
   @DisplayName("[異常系] stoneオブジェクトが異なる型のオブジェクトと比較される場合、equalsでfalseを返す")
   void testStoneEqualsDifferentObjectType() {
     // 異なる型とStoneオブジェクトを比較
-    StoneColor testcolor = StoneColor.BLACK;
-    Vector2D testposition = new Vector2D(1, 1);
-    Stone teststone = new Stone(testcolor, testposition);
+    StoneColor testColor = StoneColor.BLACK;
+    Vector2D testPosition = new Vector2D(1, 1);
+    Stone testStone = new Stone(testColor, testPosition);
 
     // 比較対象のStringオブジェクト
     String notStoneObject = "Not a Stone";
 
     // StoneオブジェクトとStringオブジェクトの比較においてequalsでfalseとなるべき
-    assertNotEquals(teststone, notStoneObject, "Stoneオブジェクトと異なる型(String)がequalsでfalseとならない");
+    assertNotEquals(testStone, notStoneObject, "stoneオブジェクトと異なる型(String)がequalsでfalseとならない");
   }
 
   @Test
-  @DisplayName("[正常系] StoneオブジェクトがhashCodeで正しくハッシュを計算する")
+  @DisplayName("[正常系] stoneオブジェクトがhashCodeで正しくハッシュを計算する")
   void testStoneHashCode() {
     // 同じ構成情報を持つstoneオブジェクトを作成
-    StoneColor testcolor = StoneColor.BLACK;
-    Vector2D testposition = new Vector2D(1, 1);
+    StoneColor testColor = StoneColor.BLACK;
+    Vector2D testPosition = new Vector2D(1, 1);
     // 比較対象1
-    Stone teststone1 = new Stone(testcolor, testposition);
+    Stone testStone1 = new Stone(testColor, testPosition);
     // 比較対象2
-    Stone teststone2 = new Stone(testcolor, testposition);
+    Stone testStone2 = new Stone(testColor, testPosition);
 
     // 2つのstoneオブジェクトにおいて同じ構成情報を持つ場合、hashCodeも一致するべき
     assertEquals(
-        teststone1.hashCode(),
-        teststone2.hashCode(),
-        "同じ構成情報を持つ2つのStoneオブジェクトのハッシュコードが一致しないため、正しくハッシュの計算ができていない");
+        testStone1.hashCode(),
+        testStone2.hashCode(),
+        "同じ構成情報を持つ2つのstoneオブジェクトのハッシュコードが一致しないため、正しくハッシュの計算ができていない");
   }
 
   @Test
   @DisplayName("[正常系] 異なる構成情報を持つ2つのstoneオブジェクトのhashCodeが異なる")
   void testStoneHashCodeDifferentObjects() {
     // 異なる構成情報を持つstoneオブジェクトを作成
-    StoneColor testcolor1 = StoneColor.BLACK;
-    StoneColor testcolor2 = StoneColor.WHITE;
-    Vector2D testposition = new Vector2D(1, 1);
-    Stone teststone1 = new Stone(testcolor1, testposition);
-    Stone teststone2 = new Stone(testcolor2, testposition);
+    StoneColor testColor1 = StoneColor.BLACK;
+    StoneColor testColor2 = StoneColor.WHITE;
+    Vector2D testPosition = new Vector2D(1, 1);
+    Stone testStone1 = new Stone(testColor1, testPosition);
+    Stone testStone2 = new Stone(testColor2, testPosition);
 
     // 2つのstoneオブジェクトにおいて構成情報が一致しない(今回は色)場合、hashCodeも一致しないべき
     assertNotEquals(
-        teststone1.hashCode(),
-        teststone2.hashCode(),
+        testStone1.hashCode(),
+        testStone2.hashCode(),
         "2つのstoneオブジェクトについて、それぞれの構成情報が異なるのに対してハッシュコードが一致するため、正しくハッシュの計算ができていない");
   }
 }
