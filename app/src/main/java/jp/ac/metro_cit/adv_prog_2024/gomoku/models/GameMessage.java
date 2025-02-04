9ce4fd3681060b5c5913f13378f6ec8febbb747a
Shrz <87812553+5hrz@users.noreply.github.com>
2025-02-04 13:53:55 +0900

    Fix/communications (#92)
    * fix: 通信相手を設定できるように修正
    
    * fix: アドレスを取得できるように変更
    
    * fix: アドレスを取得できるように変更

--- app/src/main/java/jp/ac/metro_cit/adv_prog_2024/gomoku/models/GameMessage.java
+++ app/src/main/java/jp/ac/metro_cit/adv_prog_2024/gomoku/models/GameMessage.java
@@ -7,4 +7,57 @@ import java.io.Serializable;
  *
  * @author A. Kokubo
  */
-public record GameMessage(Serializable data) implements Serializable {}
+public class GameMessage implements Serializable {
+  private final Serializable data;
+  private String source;
+  private int sourcePort;
+  private String target;
+  private int targetPort;
+
+  public GameMessage(Serializable data) {
+    this.data = data;
+  }
+
+  public GameMessage(Serializable data, String source, int sourcePort, String target, int targetPort) {
+    this.data = data;
+    this.source = source;
+    this.sourcePort = sourcePort;
+    this.target = target;
+  }
+
+  public int sourcePort() {
+    return sourcePort;
+  }
+
+  public int targetPort() {
+    return targetPort;
+  }
+
+  public Serializable data() {
+    return data;
+  }
+
+  public String source() {
+    return source;
+  }
+
+  public String target() {
+    return target;
+  }
+
+  public void setTarget(String target) {
+    this.target = target;
+  }
+
+  public void setSource(String source) {
+    this.source = source;
+  }
+
+  public void setSourcePort(int sourcePort) {
+    this.sourcePort = sourcePort;
+  }
+
+  public void setTargetPort(int targetPort) {
+    this.targetPort = targetPort;
+  }
+}
